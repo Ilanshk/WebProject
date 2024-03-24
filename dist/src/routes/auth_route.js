0feb4ed0d9035be5a636ce9d867de97c153206d2
Ilanshk <93129754+Ilanshk@users.noreply.github.com>
2024-03-24 22:58:39 +0200

    Adding swagger documentation to auth and student APIs

--- dist/src/routes/auth_route.js
+++ dist/src/routes/auth_route.js
@@ -6,9 +6,129 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
 const auth_controller_1 = __importDefault(require("../controllers/auth_controller"));
+/**
+* @swagger
+* tags:
+*   name: Auth
+*   description: The Authentication API
+*/
+/**
+* @swagger
+* components:
+*   securitySchemes:
+*     bearerAuth:
+*       type: http
+*       scheme: bearer
+*       bearerFormat: JWT
+*/
+/**
+* @swagger
+* components:
+*   schemas:
+*     User:
+*       type: object
+*       required:
+*         - email
+*         - password
+*       properties:
+*         email :
+*           type: string
+*           description: The user email
+*         password:
+*           type: string
+*           description: The user password
+*       example:
+*         email: 'bob@gmail.com'
+*         password: '123456'
+*     Tokens:
+*       type: object
+*       required:
+*         - accessToken
+*         - refreshToken
+*       properties:
+*         accessToken:
+*           type: string
+*           description: The JWT access token
+*         refreshToken:
+*           type: string
+*           description: The JWT refresh token
+*       example:
+*         accessToken: '123cd123x1xx1'
+*         refreshToken: '134r2134cr1x3c'
+*/
+/**
+* @swagger
+* /auth/login:
+*   post:
+*     summary: login existing user by email and password
+*     tags: [Auth]
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             $ref: '#/components/schemas/User'
+*     responses:
+*       200:
+*         description: The access & refresh tokens
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/Tokens'
+*/
 router.post("/login", auth_controller_1.default.login);
+/**
+* @swagger
+* /auth/register:
+*   post:
+*     summary: registers a new user
+*     tags: [Auth]
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             $ref: '#/components/schemas/User'
+*     responses:
+*       200:
+*         description: The new user
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/User'
+*/
 router.post("/register", auth_controller_1.default.register);
-router.post("/logout", auth_controller_1.default.logout);
+/**
+* @swagger
+* /auth/logout:
+*   get:
+*     summary: logout a user
+*     tags: [Auth]
+*     description: need to provide the refresh token in the auth header
+*     security:
+*       - bearerAuth: []
+*     responses:
+*       200:
+*         description: logout completed successfully
+*/
+router.get("/logout", auth_controller_1.default.logout);
+/**
+* @swagger
+* /auth/refresh:
+*   get:
+*     summary: get  new access and refresh tokens using the original refresh token
+*     tags: [Auth]
+*     description: need to provide the refresh token in the auth header
+*     security:
+*       - bearerAuth: []
+*     responses:
+*       200:
+*         description: The access and refresh tokens
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/Tokens'
+*/
 //Route to get a new refresh token
 //We do not send anything in the HTTP body request
 //The refresh token is in the header of the HTTP request
--- dist/src/routes/student_route.js
+++ dist/src/routes/student_route.js
@@ -13,8 +13,129 @@ const auth_middleware_1 = __importDefault(require("../common/auth_middleware"));
 //we use bind to connect the function to its object
 //First, authMiddleware is running to check if user is authenticated. If this function calls next()
 //then the function which is passed as the 3rd argument will be executed
+/**
+* @swagger
+* tags:
+*   name: Student
+*   description: The Student API
+*/
+/**
+* @swagger
+* components:
+*   securitySchemes:
+*     bearerAuth:
+*       type: http
+*       scheme: bearer
+*       bearerFormat: JWT
+*/
+/**
+* @swagger
+* components:
+*   schemas:
+*     Student:
+*       type: object
+*       required:
+*         - _id
+*         - name
+*         - age
+*       properties:
+*         _id:
+*           type: string
+*           description: The student's identifier
+*         name:
+*           type: string
+*           description: The student's name
+*         age:
+*           type: Number
+*           description: The student's age
+*       example:
+*         _id: 'd9j3nhj4jdi29sjqosnbs'
+*         name: 'Ilan'
+*         age: 22
+*/
+/**
+* @swagger
+* /student:
+*   get:
+*     summary: Get all Students existing or all students according to a specific requirement
+*     tags: [Student]
+*     security:
+*       - bearerAuth: []
+*     responses:
+*       200:
+*         description: list of all the students
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 $ref: '#/components/schemas/Student'
+*       401:
+*         description: Missing Token
+*       403:
+*         description: Token is not valid
+*       404:
+*         description: student was not found
+*/
 router.get("/", auth_middleware_1.default, student_controller_1.default.get.bind(student_controller_1.default));
+/**
+* @swagger
+* /student/{id}:
+*   get:
+*     summary: Get a student according to its id given as a parameter
+*     tags: [Student]
+*     security:
+*       - bearerAuth: []
+*     parameters:
+*       - in: 'path'
+*         name: 'id'
+*         required: true
+*         schema:
+*           type: 'string'
+*           example: '12345'
+*         description: Unique ID of the student to retrieve
+*     responses:
+*       200:
+*         description: A student with ID passed as a parameter
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/Student'
+*       401:
+*         description: Missing Token
+*       403:
+*         description: Invalid Token
+*       404:
+*         description: Student was not found
+*/
 router.get("/:id", auth_middleware_1.default, student_controller_1.default.getById.bind(student_controller_1.default));
+/**
+* @swagger
+* /student:
+*   post:
+*     summary: Create a new Student
+*     tags: [Student]
+*     security:
+*       - bearerAuth: []
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             $ref: '#/components/schemas/Student'
+*     responses:
+*        201:
+*          description: Student created successfully
+*          content:
+*            application/json:
+*              schema:
+*                $ref: '#/components/schemas/Student'
+*        401:
+*          description: Missing Token
+*        403:
+*          description: Invalid Token
+*
+*/
 router.post("/", auth_middleware_1.default, student_controller_1.default.post.bind(student_controller_1.default));
 router.put("/:id", auth_middleware_1.default, student_controller_1.default.put.bind(student_controller_1.default));
 router.delete("/:id", auth_middleware_1.default, student_controller_1.default.remove.bind(student_controller_1.default));
